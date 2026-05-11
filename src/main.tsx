d314b7f30c51abc260d10ac26b76646f66d2c653
workonlly <workonlly335@gmail.com>
2026-05-11 11:13:11 +0530

    admin done

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {Routes,Route, BrowserRouter} from 'react-router-dom'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
