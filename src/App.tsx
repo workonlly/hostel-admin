31398b19f69299ca6982fd0bfd3b8cdd34f4647b
workonlly <workonlly335@gmail.com>
2026-05-11 11:16:15 +0530

    react setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { NavLink } from 'react-router'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +29,7 @@ function App() {
         >
           Count is {count}
         </button>
+        <NavLink to="/sample">Go to Sample</NavLink>
       </section>
 
       <div className="ticks"></div>
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import Sample from './sample.tsx'
 import {Routes,Route, BrowserRouter} from 'react-router-dom'
 
 
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/sample" element={<Sample />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
